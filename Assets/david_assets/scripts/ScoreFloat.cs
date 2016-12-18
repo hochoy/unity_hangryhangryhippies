50619c150b47e8214452cd2e9c99267a82ef5d32
hochoy <choywaiho1@gmail.com>
2016-12-18 00:30:50 -0800

    destroy damage text after 3 seconds

--- Assets/david_assets/scripts/ScoreFloat.cs
+++ Assets/david_assets/scripts/ScoreFloat.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class ScoreFloat : MonoBehaviour {
 
 	public float floatSpeed = 0.1f;
+	private float timer = 0f;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,7 +12,12 @@ public class ScoreFloat : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		timer += Time.deltaTime;
 		Vector3 newPos = transform.position + new Vector3 (0, 10, 0);
 		transform.position = Vector3.MoveTowards (transform.position,newPos, floatSpeed);
+		if (timer > 3) {
+			Debug.Log ("destroy damage text");
+			Destroy (this.gameObject);
+		}
 	}
 }
