66afc1589371a17852466bba3cf85e1837679c26
hochoy <choywaiho1@gmail.com>
2017-03-05 20:21:48 -0800

    update meta files

--- Assets/david_assets/scripts/playerMove.cs
+++ Assets/david_assets/scripts/playerMove.cs
@@ -37,12 +37,12 @@ public class playerMove : MonoBehaviour
 
         if (Input.GetKey(KeyCode.S))
             transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+		
+		if (Input.GetKey(KeyCode.A))
+			transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
 
-        if (Input.GetKey(KeyCode.A))
-            transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
-
-        if (Input.GetKey(KeyCode.D))
-            transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+		if (Input.GetKey(KeyCode.D))
+			transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
 
     }
 
